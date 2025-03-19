41563ca066722621b09365d76490bc32ef87f0dc
Anton <antonkochergin4@gmail.com>
2025-03-19 10:42:15 +0400

    Change functional Fib

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 
 int fib(int n) {
-	if (n == 1 || n == 2) {
+	if (n == 1) {
+		return 0;
+	}
+	if (n == 2) {
 		return 1;
 	}
-	else {
-		return fib(n - 1) + fib(n - 2);
-	}
+	return fib(n - 1) + fib(n - 2);
 }
 
 int main() {
